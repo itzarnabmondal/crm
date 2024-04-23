656ae16d764ef85694be26f5059cc209f2afd908
Arnab Mondal <110527370+arxmondal@users.noreply.github.com>
2024-04-24 01:11:53 +0530

    Fix++

---
+++ frontend/generated/flow/chunks/chunk-3d14aae2236fdb9e391ccdbc3dabfbcc9426441873b774a371d400a5e22bc708.js
@@ -0,0 +1,9 @@
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/app-layout/src/vaadin-app-layout.js';
+import '@vaadin/tooltip/src/vaadin-tooltip.js';
+import '@vaadin/button/src/vaadin-button.js';
+import 'Frontend/generated/jar-resources/buttonFunctions.js';
+import '@vaadin/charts/src/vaadin-chart.js';
+import '@vaadin/vertical-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/horizontal-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/app-layout/src/vaadin-drawer-toggle.js';
